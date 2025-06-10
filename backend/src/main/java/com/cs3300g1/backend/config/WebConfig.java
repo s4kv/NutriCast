f41e60caf553b6ff67b5e12e96909e1ba12cd9b6
Rexxwell <152246711+Rexxwell@users.noreply.github.com>
2025-06-10 18:22:04 -0400

    Connected the frontend, backend, and database. Did some basic testing to make sure it works.

---
+++ backend/src/main/java/com/cs3300g1/backend/config/WebConfig.java
@@ -0,0 +1,20 @@
+/**
+ * Enables CORS in Spring Boot.
+ * This configuration allows the React Native app to call the Spring Boot backend.
+ */
+
+package com.cs3300g1.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // React Native app URL, * means allow requests from any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }    
+}
---
+++ frontend/app/(tabs)/_layout.tsx
@@ -0,0 +1,32 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#888',
+        tabBarStyle: {
+          height: 60,
+          paddingBottom: Platform.OS === 'ios' ? 10 : 0,
+          backgroundColor: '#fff',
+        },
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="welcome"
+        options={{
+          title: 'Welcome',
+        }}
+      />
+    </Tabs>
+  );
+}
