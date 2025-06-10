80b05141da70f1f3c1a4ac26fc7edc21e3cab78a
Rexxwell Putra Tendean <152246711+Rexxwell@users.noreply.github.com>
2025-06-10 13:58:03 -0400

    Add SpringBoot Files.

---
+++ nutricast/nutricast/src/test/java/com/cs3300g1/nutricast/NutricastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs3300g1.nutricast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
