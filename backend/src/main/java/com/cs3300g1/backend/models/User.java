64db9109d56caec93c64d99792ea5534b02be577
Rexxwell <152246711+Rexxwell@users.noreply.github.com>
2025-06-29 22:25:05 -0400

    Created Food and User model to represent food items and users in NutriCast and connected it to MongoDB.

--- backend/src/main/java/com/cs3300g1/backend/models/User.java
+++ backend/src/main/java/com/cs3300g1/backend/models/User.java
@@ -3,51 +3,34 @@ package com.cs3300g1.backend.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * This class represents a user in NutriCast.
+ * The user will include fields such as id, username, password, email, and list of food that the user has logged.
+ * 
+ * NOTE:
+ * - We have implemented user authentication using firebase, but to track the number of calories consumed by the user,
+ *   we will need an attribute of an list of food that the user has logged. I am sure there is a way to create a custom user model in firebase,
+ *   but for now, I just want to test the functionality when the user logs a food and that it updates the dashboard.
+ * - For now, I will be storing the password as plain text, but in the future, if we are going to implement this user model in mongoDB,
+ *   we should hash the password before storing it.
+ */
 @Document("users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-  @Id
-  private String id;
-  private String username;
-  private String email;
-  private String password;
-
-  public User(String id, String username, String email, String password) {
-    super();
-    this.id = id;
-    this.username = username;
-    this.email = email;
-    this.password = password;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    @Id
+    private String id; // Unique identifier for the user
+    private String username; // Username of the user
+    private String password; // Password of the user
+    private String email; // Email of the user
+    private List<String> loggedFoodIds; // List of foods that the user has logged
 }
