06726a9f1e277379ea411519993ae3847f95e92d
Rexxwell <152246711+Rexxwell@users.noreply.github.com>
2025-06-29 22:25:05 -0400

    Created Food and User model to represent food items and users in NutriCast and connected it to MongoDB.

--- backend/src/main/java/com/cs3300g1/backend/BackendApplication.java
+++ backend/src/main/java/com/cs3300g1/backend/BackendApplication.java
@@ -1,11 +1,19 @@
 package com.cs3300g1.backend;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.cs3300g1.backend.repositories.FoodRepository;
 import com.cs3300g1.backend.repositories.UserRepository;
+import com.cs3300g1.backend.models.User;
+import com.cs3300g1.backend.models.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -15,16 +23,17 @@ public class BackendApplication {
     SpringApplication.run(BackendApplication.class, args);
   }
 
+  /**
+   * This is used to test the connection between Springboot and MongoDB.
+   */
   @Bean
-  CommandLineRunner verify(UserRepository repo) {
+  CommandLineRunner testBackendAndDatabase(FoodRepository foods, UserRepository users) {
     return args -> {
-      if (repo.count() == 0) {
-        System.out.println("No users found in the database.");
-      } else {
-        System.out.println("Users found in the database:");
-        repo.findAll().forEach(user -> System.out.println(user.getUsername()));
-      }
+      User user = users.findByEmail("test@test.com")
+        .orElseThrow(() -> new RuntimeException("User not found"));
+      Food food = foods.findById(user.getLoggedFoodIds().get(0))
+        .orElseThrow(() -> new RuntimeException("Food not found"));
+      System.out.println("food: " + food.getName() + ", calories: " + food.getMacros().getCalories());
     };
   }
-
 }
