f2a592ac6e0080e7cc91e98c66ba3f6f475e3316
Rexxwell <152246711+Rexxwell@users.noreply.github.com>
2025-07-04 21:30:56 -0400

    When a user makes an account from firebase, it also adds the user's account information to mongoDB.

--- frontend/app/(auth)/signup.tsx
+++ frontend/app/(auth)/signup.tsx
@@ -7,6 +7,7 @@ import { View, TextInput, Button, Text, StyleSheet, Alert } from "react-native";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";  
 import { useRouter } from "expo-router";
+import backend from "../backend";
 
 export default function SignupScreen() {
   const [email, setEmail] = useState("");
@@ -15,8 +16,27 @@ export default function SignupScreen() {
 
   const handleSignup = async () => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password); // Create user in firebase
       console.log("User signed up:", userCredential.user);
+
+      // Send the user data to mongoDB.
+      try {
+        // Arguments of UserSignUpRequest.java in the backend.
+        const requestBody = {
+          email: email,
+          password: password
+        };
+
+        // Send data to backend to save the user's account information to MongoDB.
+        const response = await backend.post(
+          "/api/users",
+          requestBody
+        );
+        console.log("Response from backend: " + response.status);
+      } catch (exception: any) {
+        console.error("Error sending image to backend: " + exception);
+      }
+
       Alert.alert("Success", "Account created successfully!");
       router.push("/login");
     } catch (error: any) {
