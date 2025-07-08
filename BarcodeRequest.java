580db22f1e427a0ef1977089e84b6c20048cbfdb
KhalidBargoti <kbargoti3@gmail.com>
2025-07-07 21:56:15 -0400

    fixing issue

--- BarcodeRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cs3300g1.backend.models;
-
-// src/main/java/com/cs3300g1/backend/models/BarcodeRequest.java
-public class BarcodeRequest {
-    private String barcode;
-    // Getters and Setters
-    public String getBarcode() { return barcode; }
-    public void setBarcode(String barcode) { this.barcode = barcode; }
-}
