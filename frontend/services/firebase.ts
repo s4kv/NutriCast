b22fa1a96c11c95a68b4e552548fd3ab78e0969d
Rexxwell <152246711+Rexxwell@users.noreply.github.com>
2025-07-16 01:22:41 -0400

    Deployed backend to Google Cloud Platform. Made testing frontend on phone possible.

---
+++ frontend/services/firebase.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { initializeAuth } from "firebase/auth";
+import { getReactNativePersistence } from "./firebaseAuth/reactNativeAsyncStorage";
+import { getStorage } from "firebase/storage";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+// maybe import the db in the future? but we will not use it for now
+// right now we are using mongodb
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGAZArAPklh9_y0C92SE0v1_fFzVf4Z8s",
+  authDomain: "nutricast-backend.firebaseapp.com",
+  projectId: "nutricast-backend",
+  storageBucket: "nutricast-backend.firebasestorage.app",
+  messagingSenderId: "694486797890",
+  appId: "1:694486797890:web:6231ac98a3116c9ee867db",
+  measurementId: "G-0LQLHPW33T",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const storage = getStorage(app);
+const firebaseAuth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
+
+// export { app, analytics, storage };
+export { app, storage, firebaseAuth };
